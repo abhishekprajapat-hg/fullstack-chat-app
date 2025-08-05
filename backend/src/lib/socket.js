60bf868855de08d854849d661d4b0c33e840a311
Abhishek Prajapat <abhi03prajapat@gmail.com>
2025-08-05 16:23:47 +0530

    Update index.js

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -1,23 +1,22 @@
 import { Server } from "socket.io";
 
-// Store userId-to-socketId mapping
 const userSocketMap = {}; // { userId: socketId }
 
-// Exported function to retrieve receiver socket ID
+let ioInstance = null; // for external access in controllers
+
 export function getReceiverSocketId(userId) {
   return userSocketMap[userId];
 }
 
-// Setup function to initialize socket.io on the passed server
 export function setupSocket(server) {
-  const io = new Server(server, {
+  ioInstance = new Server(server, {
     cors: {
       origin: ["http://localhost:5173"],
       credentials: true,
     },
   });
 
-  io.on("connection", (socket) => {
+  ioInstance.on("connection", (socket) => {
     console.log("🟢 A user connected:", socket.id);
 
     const userId = socket.handshake.query.userId;
@@ -25,15 +24,19 @@ export function setupSocket(server) {
       userSocketMap[userId] = socket.id;
     }
 
-    // Notify all clients of online users
-    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+    ioInstance.emit("getOnlineUsers", Object.keys(userSocketMap));
 
     socket.on("disconnect", () => {
       console.log("🔴 A user disconnected:", socket.id);
       if (userId) {
         delete userSocketMap[userId];
-        io.emit("getOnlineUsers", Object.keys(userSocketMap));
+        ioInstance.emit("getOnlineUsers", Object.keys(userSocketMap));
       }
     });
   });
 }
+
+export const io = {
+  to: (...args) => ioInstance?.to(...args),
+  emit: (...args) => ioInstance?.emit(...args),
+};
